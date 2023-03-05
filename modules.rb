d330520fd2a61dacba75921edd3905f7d6c68de7
codingvaleria <codingvaleria@gmail.com>
2023-03-05 13:05:25 +0300

    Create module and call them within themselves

---
+++ modules.rb
@@ -0,0 +1,14 @@
+# Modules are a container for storing a group of methods
+#helper functions
+
+module Tools
+    def say_hi(name)
+        puts "hi " + name
+    end
+
+    def say_bye(name)
+        puts "bye " + name
+    end
+
+end
+
