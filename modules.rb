fca85711c418de538df03c78e595731c3a9b1f9f
codingvaleria <codingvaleria@gmail.com>
2023-03-24 08:07:22 +0300

    More on modules

--- modules.rb
+++ modules.rb
@@ -1,48 +1,64 @@
-# Modules are a container for storing a group of methods
-#helper functions
+# # Modules are a container for storing a group of methods
+# #helper functions
 
-module Tools
-    def say_hi(name)
-        puts "hi " + name
+# module Tools
+#     def say_hi(name)
+#         puts "hi " + name
+#     end
+
+#     def say_bye(name)
+#         puts "bye " + name
+#     end
+
+# end
+
+# #accessing methods within the module
+# include Tools
+# Tools.say_bye("Mike")
+# Tools.say_hi("Nonni")
+
+# #Nesting Modules
+# module Sale
+#     module FormBuilder
+#         class << self
+#             def call(params)
+#                 subtotal = params[:subtotal]
+#                 state_name = params[:state_name]
+
+#                 tax_amount = subtotal * self.tax_rate(state_name)
+#                 subtotal + tax_amount
+#             end
+
+#             private
+#             def tax_rate(state)
+#                 if state == "AZ"
+#                     0.065
+#                 elsif state == "CA"
+#                     0.087
+#                 end
+#             end
+#         end
+#     end
+# end
+
+# form_data  = {
+#     subtotal: 5.5,
+#     state_name: "AZ"
+# }
+# Sale::FormBuilder.call(form_data)
+
+module SayHello
+    def say_hello
+        puts "Hello #{@name}"
     end
-
-    def say_bye(name)
-        puts "bye " + name
-    end
-
 end
 
-#accessing methods within the module
-include Tools
-Tools.say_bye("Mike")
-Tools.say_hi("Nonni")
-
-#Nesting Modules
-module Sale
-    module FormBuilder
-        class << self
-            def call(params)
-                subtotal = params[:subtotal]
-                state_name = params[:state_name]
-
-                tax_amount = subtotal * self.tax_rate(state_name)
-                subtotal + tax_amount
-            end
-
-            private
-            def tax_rate(state)
-                if state == "AZ"
-                    0.065
-                elsif state == "CA"
-                    0.087
-                end
-            end
-        end
+class Person
+    include SayHello
+    extend SayHello
+    def initialize name
+        @name = name
     end
 end
 
-form_data  = {
-    subtotal: 5.5,
-    state_name: "AZ"
-}
-Sale::FormBuilder.call(form_data)
+person = Person.new "Sebastian"
