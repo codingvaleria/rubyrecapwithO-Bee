2f2ac2fb700674606f7daff6a6a0494ab835515a
codingvaleria <codingvaleria@gmail.com>
2023-03-24 09:37:05 +0300

    Inheritance ans super

---
+++ inheritance.rb
@@ -0,0 +1,67 @@
+#Ruby offers only Single inheritance - modules provide a solution to this, when some behaviours are not adopted by some classes.
+
+#How ruby calls methods
+#call the ancestors method - works from down-up
+
+#polymorphism - derived classes overule behaviours in parent class
+
+#super is used when you want to call the method on parent class
+
+
+class House
+    def has_roof
+        "I have a roof"
+    end
+
+    def has_floor
+        "I have a floor"
+    end
+    
+end
+
+
+class Bungalow < House
+    def has_floor
+        puts super
+        "i have red floors"
+    end
+end
+
+
+b1 = Bungalow.new
+puts b1.has_floor
+
+#when including modules, they take priority over super class
+
+module Property
+    def has_floor
+        "floor has to be tiled"
+    end
+
+end
+
+class House
+    def has_roof
+        "I have a roof"
+    end
+
+    def has_floor
+        "I have a floor"
+    end
+    
+end
+
+
+class Bungalow < House
+include Property
+    def has_floor
+        puts super
+        "i have red floors"
+    end
+end
+
+
+b1 = Bungalow.new
+puts b1.has_floor
+
+
